e831e2f555cd6880ed8d0b97c21fc5cba153a02d
Vladislav <bllxy67@gmail.com>
2024-08-03 20:30:00 +0300

    feat: add global styles

---
+++ src/app/styles/index.ts
@@ -0,0 +1,6 @@
+export * from './font.css';
+export * from './global.css';
+export * from './reset.css';
+export * from './spacing.css';
+export * from './themes.css';
+export * from './transition.css';
