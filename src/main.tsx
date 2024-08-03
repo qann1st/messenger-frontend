09b91b38c34fd7146e8d8151e1d40ac5f6a278d3
Vladislav <bllxy67@gmail.com>
2024-08-03 20:33:47 +0300

    feat: add main

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+
+import { App } from '~/app';
+
+const root = document.getElementById('root');
+if (!root) {
+  throw new Error('Root element not found');
+}
+
+ReactDOM.createRoot(root).render(<App />);
